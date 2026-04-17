217e2b6051dc3fd53efb8321e29b6b4194aaf8b3
summ <smm979@163.com>
2026-04-17 17:19:43 +0800

    增加水果品类

--- pages/cost/cost.vue
+++ pages/cost/cost.vue
@@ -122,7 +122,7 @@ import { ref, computed } from 'vue'
 import { onShow } from '@dcloudio/uni-app'
 
 // ---- 核心状态 ----
-const categories = ['蔬菜', '肉蛋', '水产', '调料', '其他']
+const categories = ['蔬菜', '水果', '肉蛋', '水产', '调料', '其他']
 const currentCategory = ref('全部')
 const list = ref([])
 
